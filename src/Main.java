80a860bb0bb2b836b43bfea87061f3390bc26c4d
621352 <camarenburt>
2024-01-26 09:10:29 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] original = {1, 2, 3, 4};
+        int x = original[original.length - 1];
+
+        for (int i = original.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                original[i] = x;
+            } else {
+                original[i] = original[i - 1];
+            }
+        }
+        for (int i = 0; i < original.length; i++) {
+            System.out.print(original[i] + " ");
+        }
     }
+
+    //public static int[] insert(int[] arr, int element, int index) {
+    //        int[] nums = arr;
+    //        nums[index] = element;
+    //        return nums;
+    //    }
 }
